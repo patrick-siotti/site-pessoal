440aae962bb85dd2b8378227162eed34376438cb
patrick siotti <59841892+patrick-siotti@users.noreply.github.com>
2022-08-01 15:17:25 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,7 @@
-TOKEN = "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" # token do github
-NOME = 'Patrick Gianluppi Siotti' # nome completo
-TEXTO = '''<p>Olá! Me chamo Patrick, sou calmo, adoro aprender e gosto de ser eficiente, trabalho com free lancer a 1 ano como programador python, tendo ganhado bastante experiencia graças a isso.</p>
-<p>Em python: Tenho conhecimento de todo o basico (bibliotecas, dicionarios, listas, if elif else, repetições, etc), tendo conhecimento de objetos e algumas bibliotecas: flask, github, selenium, telegram, discord, etc. E muitas bibliotecas já incluidas no python como: os, time, random, etc.</p>
-<p>Em web: Em criação web, esotou mais familiarizado com o front end, gosto muito de brincar com css. Em html5 tenho conhecimento de todo o basico e a logica por tras, no css3 tenho conhecimento basico sobre tudo, mas se precisso fazer algo a mais, não fico intimidado, e vou atras fazer.</p>
-<p>Estou estudando atualmente engenharia de dados, gosto muito dessa área e quero aproveitar o maximo dela.</p>
-<p>esse site foi feito usando html, css e python</p>''' # texto de apresentação
+TOKEN = "SEU TOKEN" # token do github
+NOME = 'SEU NOME' # nome completo
+TEXTO = '''<h1>TEXTO DE APRESENTAÇÃO</h1>
+<p>pode usar html (recomendado)</p>''' # texto de apresentação
 
 IGNORAR = ['zip', 'ttf', 'otf', 'db', 'psd'] # arquivos que não serão abertos
 IMAGEM = ['jpeg', 'jpg', 'gif', 'png', 'bmp', 'tiff', 'pdf', 'svg', 'raw', 'webp', 'ico', 'icon', 'psd'] # arquivos de imagem
